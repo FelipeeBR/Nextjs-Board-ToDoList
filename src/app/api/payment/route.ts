df722bb6ae5598841864b57163426f4ce10c805a
FelipeeBR <felipemen74@gmail.com>
2024-01-19 20:37:47 -0300

    voltando uma parte

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -6,6 +6,7 @@ export async function POST(req: NextRequest) {
     try {
         const dados = await req.json();
         const chave = dados.produtos.chave;
+        console.log(dados);
         console.log(chave);
 
         /*if(chave !== "3f842492bf58a2c5b5150eed082a04e4"){
@@ -23,7 +24,7 @@ export async function POST(req: NextRequest) {
             console.log('A compra não foi finalizada. Ignorando.');
         }
 
-        return NextResponse.json({ message:"informação recebida com sucesso."}, {status: 200});
+        return NextResponse.json({ message:"informação recebida com sucesso.", dados: dados}, {status: 200});
     } catch (error) {
         return NextResponse.json({ message:"Algo deu errado."}, {status: 500});
     }
