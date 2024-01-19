89275bbe4d5c6d232ff00a5145aa3fb9bfa66700
FelipeeBR <felipemen74@gmail.com>
2024-01-19 15:48:17 -0300

    teste postback

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -3,8 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     try {
-        const dados = req.body;
+        const dados = await req.json();
+        //await req.json().then((data) => data as {data: {id: string}});
         console.log('Postback recebido:', dados);
+
+        const chave_unica = dados?.getReader
         return NextResponse.json({ message:"informação recebida com sucesso.", dados: dados}, {status: 200});
     } catch (error) {
         return NextResponse.json({ message:"Algo deu errado."}, {status: 500});
