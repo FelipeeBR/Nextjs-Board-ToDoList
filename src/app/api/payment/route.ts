2e78eaf84c6dd9fc1a4403e1f9cd0a5f9bf10eba
FelipeeBR <felipemen74@gmail.com>
2024-01-19 15:16:07 -0300

    postback testes

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -5,7 +5,7 @@ export async function POST(req: NextRequest) {
     try {
         const dados = req.body;
         console.log('Postback recebido:', dados);
-        return NextResponse.json({ message:"informação recebida com sucesso."}, {status: 200});
+        return NextResponse.json({ message:"informação recebida com sucesso.", dados: dados}, {status: 200});
     } catch (error) {
         return NextResponse.json({ message:"Algo deu errado."}, {status: 500});
     }
