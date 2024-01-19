04285dc6c0f9fb787c22ead1fdb519909e9cfa6d
FelipeeBR <felipemen74@gmail.com>
2024-01-19 16:16:56 -0300

    testando postback

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -4,10 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     try {
         const dados = await req.json();
-        //await req.json().then((data) => data as {data: {id: string}});
         console.log('Postback recebido:', dados);
 
-        const chave_unica = dados?.getReader
+        const statusVenda = dados.venda.status;
+        if (statusVenda === 'Finalizada') {
+            console.log('A compra foi finalizada. Executar lógica adicional se necessário.');
+        } else {
+            console.log('A compra não foi finalizada. Ignorando.');
+        }
+
         return NextResponse.json({ message:"informação recebida com sucesso.", dados: dados}, {status: 200});
     } catch (error) {
         return NextResponse.json({ message:"Algo deu errado."}, {status: 500});
