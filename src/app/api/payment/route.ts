3649234ab3d6667c6be48160e8c3ae3b8aafe616
FelipeeBR <felipemen74@gmail.com>
2024-01-19 17:44:17 -0300

    testando postback

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -5,15 +5,13 @@ export async function POST(req: NextRequest) {
     try {
         const dados = await req.json();
 
-        const statusVenda = dados.venda.status;
-        console.log(statusVenda);
-        if (statusVenda === 'Finalizada') {
-            console.log('A compra foi finalizada. Executar lógica adicional se necessário.');
-        } else {
-            console.log('A compra não foi finalizada. Ignorando.');
-        }
+        var filter = dados.venda.filter(function(e:any) {
+            var status = e.status
+            return status;
+        });
+        console.log(filter);
 
-        return NextResponse.json({ message:"informação recebida com sucesso.", dados: dados}, {status: 200});
+        return NextResponse.json({ message:"informação recebida com sucesso."}, {status: 200});
     } catch (error) {
         return NextResponse.json({ message:"Algo deu errado."}, {status: 500});
     }
