20a9711848cd3c55b107aa3403df72e0dc89cee2
FelipeeBR <felipemen74@gmail.com>
2024-01-19 18:18:15 -0300

    visualizando dados cliente

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,13 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import { exit } from "process";
 
 
 export async function POST(req: NextRequest) {
     try {
         const dados = await req.json();
+        const chave = dados.produtos.chave;
+
+        if(chave !== "3f842492bf58a2c5b5150eed082a04e4"){
+            exit();
+        }
 
         const statusVenda = dados.venda.status;
         if (statusVenda === 'Finalizada') {
-            console.log('A compra foi finalizada. Executar lógica adicional se necessário.');
+            console.log('A compra foi finalizada.');
+            let email = dados.produtos.comprador.email;
+            let cnpj_cpf = dados.produtos.comprador.cnpj_cpf;
+            console.log(email);
+            console.log(cnpj_cpf);
         } else {
             console.log('A compra não foi finalizada. Ignorando.');
         }
