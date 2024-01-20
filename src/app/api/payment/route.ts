02032058d4a62490c34d3c75a42cb0ef2b2d088d
FelipeeBR <felipemen74@gmail.com>
2024-01-19 21:03:22 -0300

    vendo status da venda

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -7,23 +7,18 @@ export async function POST(req: NextRequest) {
         const dados = await req.json();
         const chave = dados.produtos.chave;
         console.log(dados);
-        console.log(chave);
 
         /*if(chave !== "3f842492bf58a2c5b5150eed082a04e4"){
             exit();
         }*/
 
         const statusVenda = dados.venda.status;
+        console.log(statusVenda);
         if (statusVenda === 'Finalizada') {
             console.log('A compra foi finalizada.');
-            let email = dados.produtos.comprador.email;
-            let cnpj_cpf = dados.produtos.comprador.cnpj_cpf;
-            console.log(email);
-            console.log(cnpj_cpf);
         } else {
             console.log('A compra não foi finalizada. Ignorando.');
         }
-
         return NextResponse.json({ message:"informação recebida com sucesso.", dados: dados}, {status: 200});
     } catch (error) {
         return NextResponse.json({ message:"Algo deu errado."}, {status: 500});
